b00e58edfca626af9902ddd650bfbcd56850dc5b
abadied <abadi.eden@gmail.com>
2018-11-20 13:55:04 +0200

    added render func

--- Server.py
+++ Server.py
@@ -1,10 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/booksmap', methods=['POST'])
 def get_books_data(books_filters):
+    pass
+
+@app.route('/main', methods=['GET'])
+def get_main_page():
+    # will render main js page
     pass
 
 def __main__():
