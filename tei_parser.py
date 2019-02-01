2b125d7647fc85fee8fd445ebc476a39c41cc49a
abadied <abadi.eden@gmail.com>
2019-02-01 13:01:44 +0200

    added new parser works on most i think - works recusivley with generators

--- tei_parser.py
+++ tei_parser.py
@@ -2,6 +2,7 @@ import pandas as pd
 import codecs
 import xmltodict
 import copy
+import re
 
 MIN_NUM_OF_COLUMNS = 3
 
@@ -29,16 +30,7 @@ def tei_parser(predicted_file_path, correct_parsed_file_path):
 
     xml_file = codecs.open(correct_parsed_file_path, 'r', 'utf8')
     xml_text = xml_file.read()
-
-    xml_dict = xmltodict.parse(xml_text)
-
-    place_list_of_dict = xml_dict['TEI']['teiHeader']['profileDesc']['settingDesc']['listPlace']['place']
-    org_list_of_dict = xml_dict['TEI']['teiHeader']['profileDesc']['particDesc']['org']
-    person_list_of_dict = xml_dict['TEI']['teiHeader']['profileDesc']['particDesc']['listPerson']['person']
-
-    wanted_place_list = [_dict['placeName'] for _dict in place_list_of_dict]
-    wanted_org_list = [_dict['name'] if 'name' in _dict.keys() else _dict['orgName'] for _dict in org_list_of_dict]
-    wanted_person_list = [_dict['persName']['forename'] + '-' + _dict['persName']['surname'] for _dict in person_list_of_dict]
+    wanted_place_list, wanted_org_list, wanted_person_list = parse_by_dictionary_inner(xml_text)
 
     print('start comparision:')
 
@@ -50,6 +42,9 @@ def tei_parser(predicted_file_path, correct_parsed_file_path):
     results_dict = {'loc': {'true': wanted_place_list, 'pred': adler_loc_list},
                     'org': {'true': wanted_org_list, 'pred': adler_org_list},
                     'pers': {'true': wanted_person_list, 'pred': adler_pers_list}}
+    negative_dict = {'loc': base_df.loc[base_df['unspecified_12'] != 'I_LOC']['base_word'].tolist(),
+                     'org': base_df.loc[base_df['unspecified_12'] != 'I_ORG']['base_word'].tolist(),
+                     'pers': base_df.loc[base_df['unspecified_12'] != 'I_PERS']['base_word'].tolist()}
 
     for list_key in results_dict.keys():
         true_list = results_dict[list_key]['true']
@@ -60,23 +55,107 @@ def tei_parser(predicted_file_path, correct_parsed_file_path):
                 if p_val in t_val:
                     conf_mat_for_type[list_key]['tp'] += 1
                     found = True
+                    break
             if not found:
                 conf_mat_for_type[list_key]['fp'] += 1
-        conf_mat_for_type[list_key]['fn'] = len(pred_list) - conf_mat_for_type[list_key]['tp']
-        conf_mat_for_type[list_key]['tn'] = base_df.shape[0] - conf_mat_for_type[list_key]['fp']
+
+        # look for false positive for each type
+        for n_val in negative_dict[list_key]:
+            for real_f_val in true_list:
+                if n_val in real_f_val:
+                    conf_mat_for_type[list_key]['fn'] += 1
+                    break
+
+        conf_mat_for_type[list_key]['fp'] = max(0, len(pred_list) - conf_mat_for_type[list_key]['tp'])
+        conf_mat_for_type[list_key]['tn'] = max(0, len(negative_dict[list_key]) - conf_mat_for_type[list_key]['fn'])
 
         # normalize
-        conf_mat_for_type[list_key]['fn'] /= len(pred_list)
+        conf_mat_for_type[list_key]['fn'] /= len(negative_dict[list_key])
         conf_mat_for_type[list_key]['tp'] /= len(pred_list)
-        conf_mat_for_type[list_key]['fp'] /= base_df.shape[0]
-        conf_mat_for_type[list_key]['tn'] /= base_df.shape[0]
+        conf_mat_for_type[list_key]['fp'] /= len(pred_list)
+        conf_mat_for_type[list_key]['tn'] /= len(negative_dict[list_key])
 
     print(conf_mat_for_type)
 
 
+def parse_brute_force(xml_text):
+    first_q = 'placeName'
+    sec_q = 'orgName'
+    third_q = 'persName'
+    parsed_by_first = xml_text.split(first_q)
+    parsed_by_sec = xml_text.split(sec_q)
+    parsed_by_third = xml_text.split(third_q)
+
+    parsed_by_first = [x.replace('/', '').replace('<', '').replace('>', '').replace('\n', '').replace('\r', '') for x in parsed_by_first]
+    parsed_by_sec = [x.replace('/', '').replace('<', '').replace('>', '').replace('\n', '').replace('\r', '') for x in parsed_by_sec]
+    parsed_by_third = [x.replace('/', '').replace('<', '').replace('>', '').replace('\n', '').replace('\r', '') for x in parsed_by_third]
+
+    del parsed_by_first[0::2]
+    del parsed_by_sec[0::2]
+    del parsed_by_third[0::2]
+    return parsed_by_first, parsed_by_sec, parsed_by_third
+
+
+def parse_by_dictionary_lists(xml_text):
+    xml_dict = xmltodict.parse(xml_text)
+
+    place_list_of_dict = xml_dict['TEI']['teiHeader']['profileDesc']['settingDesc']['listPlace']['place']
+    org_list_of_dict = xml_dict['TEI']['teiHeader']['profileDesc']['particDesc']['org']
+    person_list_of_dict = xml_dict['TEI']['teiHeader']['profileDesc']['particDesc']['listPerson']['person']
+
+    wanted_place_list = [_dict['placeName'] for _dict in place_list_of_dict]
+    wanted_org_list = [_dict['name'] if 'name' in _dict.keys() else _dict['orgName'] for _dict in org_list_of_dict]
+    wanted_person_list = [_dict['persName']['forename'] + '-' + _dict['persName']['surname'] for _dict in person_list_of_dict]
+
+    return wanted_place_list, wanted_org_list, wanted_person_list
+
+
+def parse_by_dictionary_inner(xml_text):
+
+    xml_dict = xmltodict.parse(xml_text)
+
+    place_list = []
+    org_list = []
+    person_list = []
+
+    def recursive_items(dictionary, upper_tag):
+        for _key, _value in dictionary.items():
+            if isinstance(_value, dict) and _key != 'teiHeader':
+                yield from recursive_items(_value, _key)
+            else:
+                if isinstance(_value, list):
+                    for v in _value:
+                        if isinstance(v, dict):
+                            yield from recursive_items(v, _key)
+                        else:
+                            yield (_key, v, upper_tag)
+                else:
+                    yield (_key, _value, upper_tag)
+
+    for key, value, upper_tag in recursive_items(xml_dict, None):
+        if '#' not in value:
+            if key == 'placeName' or upper_tag == 'placeName':
+                value_list = get_splitted_value_list(value)
+                place_list.extend(value_list)
+            elif key == 'orgName' or upper_tag == 'orgName':
+                value_list = get_splitted_value_list(value)
+                org_list.extend(value_list)
+            elif key == 'persName' or key == 'surName' or key == 'foreName' or upper_tag == 'persName':
+                value_list = get_splitted_value_list(value)
+                person_list.extend(value_list)
+
+    return place_list, org_list, person_list
+
+
+def get_splitted_value_list(value):
+    value_list = re.split("[, \-!?:]+", value)
+    value_list = [x for x in value_list if x != '' and x != '.' and x != ' ']
+    return value_list
+
+
 def main():
-    predicted_file_path = 'adler_parser.txt'
-    true_labeled_file_path = 'ass1/Untitled1.xml'
+    predicted_file_path = 'adler_files/116_אלמגור_דן.txt'
+    true_labeled_file_path = 'student_files/XML-DanAlmagor.xml'
     tei_parser(predicted_file_path, true_labeled_file_path)
 
 
